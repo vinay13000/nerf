e7e1477828610800d81fc98135b23c7aa56d70ed
vinay13000 <74045128+vinay13000@users.noreply.github.com>
2021-10-20 19:46:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+ function setup(){
+     canvas=createCanvas(300,350);
+     canvas.center();
+     video=createCapture(VIDEO);
+     video.hide();
+     classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/D9EwuODbR/model.json',modelLoaded);
+
+
+ }
+
+ function draw(){
+  image(video,0,0,300,350)
+  classifier.classify(video,gotResult)
+ }
+
+ function modelLoaded(){
+     console.log("modelLoaded");
+
+ }
+
+ function gotResult(error,results){
+     if(error){
+         console.error(error);
+
+     }
+     else{
+         console.log(results);
+         document.getElementById("resut_object_name").innerHTML=results[0].label;
+         document.getElementById("resut_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+     }
+ }
+
+ 
